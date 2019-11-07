f1fb5557f8ced13610e63b7becca02632d83eb23
Christian RS <christian.romero@rosinet.com.mx>
2019-11-07 17:50:58 -0600

    iniciando sincronizacion

---
+++ 01.js
@@ -0,0 +1,23 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Hola mundo con Vue',
+        frutas: [
+            {nombre: 'Pera', cantidad: 10},
+            {nombre: 'Manzana', cantidad: 8},
+            {nombre: 'Platano', cantidad: 1},
+            {nombre: 'Uvas', cantidad: 0}
+        ],
+        nuevaFruta: ''
+    },
+    methods: {
+      agregarFruta (){
+        this.frutas.push({
+            nombre: this.nuevaFruta, cantidad: 0
+        })
+      },
+      otroMetodo (){
+
+      }  
+    }, 
+})
